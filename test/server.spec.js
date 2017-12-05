c52085a1025b88d65bd73f1b5d51af43c104a168
Alex Mercier <pro.alexandre.mercier@gmail.com>
2017-12-05 14:10:08 +0100

    Updated tests

--- test/server.spec.js
+++ test/server.spec.js
@@ -1,32 +1,77 @@
 const { it, describe } = require('mocha');
 const { expect } = require('chai');
 const { get } = require('axios');
-const startServer = require('../lib/server');
+const express = require('express');
+const { join } = require('path');
+const proxyquire = require('proxyquire');
 
-const port = 4000;
-const baseURL = `http://localhost:${port}`;
+let bingApiStub = null;
+proxyquire('../lib/server', {
+  'node-bing-api': (...args) => bingApiStub(...args),
+});
+const makeApp = require('../lib/server');
 
-describe('server.js', () => {
-  const url = `${baseURL}/`;
+const testPort = 4000;
+const testBaseURL = `http://localhost:${testPort}`;
+const dataPort = 5000;
+const dataBaseURL = `http://localhost:${dataPort}`;
+
+function listen(app, port) {
+  return new Promise((resolve, reject) => {
+    const server = app.listen(port, err => (err && reject(err)) || resolve(server));
+  });
+}
+
+function makeSuccessfulBingImageApiStub(body) {
+  return () => ({
+    images: (q, options, callback) => {
+      setTimeout(() => {
+        callback(null, { body: JSON.stringify(body) }, body);
+      }, 0);
+    },
+  });
+}
+
+describe('lib/server.js', () => {
+  const url = `${testBaseURL}/`;
+  const apiKey = 'API KEY STUB';
 
   let server;
+  let dataServer;
 
-  beforeEach(() => {
-    server = startServer();
-    server.listen(port);
+  beforeEach(async () => {
+    const app = makeApp({ apiKey });
+    server = await listen(app, testPort);
+
+    const dataApp = express();
+    dataApp.use(express.static(join(__dirname, '/data')));
+    dataServer = await listen(dataApp, dataPort);
   });
 
-  it('should return 200', async () => {
-    const { status } = await get(url);
-    expect(status).to.equal(200);
-  });
+  describe('when API returns one jpeg image', () => {
+    beforeEach(() => {
+      bingApiStub = makeSuccessfulBingImageApiStub({
+        value: [{
+          contentUrl: `${dataBaseURL}/kitten.jpg`,
+          encodingFormat: 'jpeg',
+          thumbnail: {},
+        }],
+      });
+    });
 
-  it('should say "Hello World"', async () => {
-    const { data } = await get(url);
-    expect(data).to.equal('Hello World\n');
+    it('should return 200', async () => {
+      const { status } = await get(url);
+      expect(status).to.equal(200);
+    });
+
+    it('should set a Content-Type header', async () => {
+      const { headers } = await get(url);
+      expect(headers['content-type']).to.equal('image/jpeg');
+    });
   });
 
   afterEach(() => {
+    dataServer.close();
     server.close();
   });
 });
