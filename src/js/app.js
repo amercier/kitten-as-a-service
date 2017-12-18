b74f8b52504a675420b7aeee43f9b21775440f87
Alex Mercier <pro.alexandre.mercier@gmail.com>
2017-12-18 07:12:36 +0100

    Added swiping support

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,7 @@
 import domready from 'domready';
 import debounce from 'lodash.debounce';
 import screenfull from 'screenfull';
+import Hammer from 'hammerjs';
 
 domready(() => {
   // Fullscreen
@@ -38,10 +39,53 @@ domready(() => {
     }
   });
 
-  body.addEventListener('click', () => {
-    if (!body.classList.contains('loading')) {
+  let ticking = false;
+
+  const startX = 0; // Math.round((window.innerWidth - body.offsetWidth) / 2);
+  let translateX = startX;
+
+  const reqAnimationFrame =
+    window[Hammer.prefixed(window, 'requestAnimationFrame')]
+    || (callback => window.setTimeout(callback, 1000 / 60));
+
+  function updateElementTransform() {
+    const value = `translate3d(${translateX}px, 0, 0)`;
+    body.style.webkitTransform = value;
+    body.style.mozTransform = value;
+    body.style.transform = value;
+    ticking = false;
+  }
+
+  function requestElementUpdate() {
+    if (!ticking) {
+      ticking = true;
+      reqAnimationFrame(updateElementTransform);
+    }
+  }
+
+  function resetElement() {
+    body.classList.add('animated');
+    translateX = startX;
+    requestElementUpdate();
+  }
+
+  const isAvailable = () => !body.classList.contains('loading');
+
+  const hammer = new Hammer(body);
+  hammer.get('pan').set({ direction: Hammer.DIRECTION_HORIZONTAL });
+  hammer.on('panstart panmove', ({ deltaX }) => {
+    if (isAvailable()) {
+      body.classList.remove('animated');
+      translateX = startX + deltaX;
+      requestElementUpdate();
+    }
+  });
+  hammer.on('pancancel panend', resetElement);
+  hammer.on('swipe', () => {
+    if (isAvailable()) {
       body.classList.remove('failed');
       body.classList.add('loading');
+      resetElement();
       loadNewPicture();
     }
   });
